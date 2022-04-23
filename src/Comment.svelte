ce0361075f1d24d6092ab8f3918880a77c731c71
Logan Woolf <loganwoolf@gmail.com>
2022-04-23 17:26:02 -0600

    Consume props in Comment component

---
+++ src/Comment.svelte
@@ -0,0 +1,25 @@
+<script>
+  export let id
+  export let content
+  export let createdAt
+  export let score
+  export let user
+  export let isReply
+
+</script>
+
+<article data-comment-id={id} class={isReply && 'reply'}>
+  <header>
+    <img src="{user.image.png}" alt="" />
+    <h2>{user.username}</h2>
+    <p>{createdAt}</p>
+  </header>
+  <p>{content}</p>
+  <footer>
+    <p>+ {score} -</p>
+    <button>Reply</button>
+  </footer>
+</article>
+
+<style>
+</style>
