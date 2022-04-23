c7df26283ffbcda2bf4b14883dfa0613af247d0d
Logan Woolf <loganwoolf@gmail.com>
2022-04-23 17:25:31 -0600

    Fetch data and loop over comments and replies

--- src/App.svelte
+++ src/App.svelte
@@ -1,65 +1,27 @@
 <script>
-  import logo from './assets/svelte.png'
-  import Counter from './lib/Counter.svelte'
+  import Comment from './Comment.svelte'
+
+  let comments
+  let currentUser
+  fetch('/data.json')
+    .then((response) => response.json())
+    .then((result) => {
+      comments = result.comments
+      currentUser = result.currentUser
+    })
+    .catch((err) => console.log('Error: ', err.message))
+    $: console.log({comments, currentUser} )
 </script>
 
-<main>
-  <img src={logo} alt="Svelte Logo" />
-  <h1>Hello world!</h1>
+{#if !comments}
+  <p>Please wait for some comments</p>
+{:else}
+  {#each comments as comment}
+    <Comment {...comment} />
+    {#each comment.replies as reply}
+      <Comment {...reply} isReply />
+    {/each}
+  {/each}
+{/if}
 
-  <Counter />
-
-  <p>
-    Visit <a href="https://svelte.dev">svelte.dev</a> to learn how to build Svelte
-    apps.
-  </p>
-
-  <p>
-    Check out <a href="https://github.com/sveltejs/kit#readme">SvelteKit</a> for
-    the officially supported framework, also powered by Vite!
-  </p>
-</main>
-
-<style>
-  :root {
-    font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen,
-      Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
-  }
-
-  main {
-    text-align: center;
-    padding: 1em;
-    margin: 0 auto;
-  }
-
-  img {
-    height: 16rem;
-    width: 16rem;
-  }
-
-  h1 {
-    color: #ff3e00;
-    text-transform: uppercase;
-    font-size: 4rem;
-    font-weight: 100;
-    line-height: 1.1;
-    margin: 2rem auto;
-    max-width: 14rem;
-  }
-
-  p {
-    max-width: 14rem;
-    margin: 1rem auto;
-    line-height: 1.35;
-  }
-
-  @media (min-width: 480px) {
-    h1 {
-      max-width: none;
-    }
-
-    p {
-      max-width: none;
-    }
-  }
-</style>
+<style></style>
