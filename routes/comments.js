92c2e7260068e355d9f35c10df9cd703bed6eba2
Logan Woolf <loganwoolf@gmail.com>
2022-04-28 14:28:15 -0600

    Create root comment query

--- routes/comments.js
+++ routes/comments.js
@@ -1,9 +1,32 @@
 import express from 'express'
 const router = express.Router()
 
-export default (db) => {
+export default (query) => {
   router.get('/', (req, res) => {
-    res.send(db.comments)
+    query('comments')
+      .select({
+        id: 'id',
+        content: 'content',
+        createdAt: 'created_at',
+        score: 'score',
+        user: 'user',
+        replyTo: 'replyTo',
+      })
+      .then((rows) => {
+        return rows.map((comment) => {
+          query('users')
+            .select({
+              username: 'username',
+              image: 'image_png',
+            })
+            .where({ id: comment.user })
+            .then((rows) => {
+              comment.user = rows[0]
+            })
+          return comment
+        })
+      })
+      .then((data) => res.json(data))
   })
 
   return router
