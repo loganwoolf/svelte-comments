4348db90cab60810ec9f0f8d03d0e52e955401e0
Logan Woolf <loganwoolf@gmail.com>
2022-04-28 01:35:10 -0600

    Reconfigure Knex to be less dumb
    Move knexfile to root so that you don't always have to cd to db
    Introduce config file to remove knex config from server.js

--- knexfile.js
+++ knexfile.js
@@ -1,53 +1,52 @@
-import path from 'path'
-const __dirname = path.resolve()
 // Update with your config settings.
 
 /**
  * @type { Object.<string, import("knex").Knex.Config> }
  */
 export default {
-
   development: {
     client: 'sqlite3',
     connection: {
-      filename: path.join(__dirname, 'dev.sqlite3')
+      filename: './db/dev.sqlite3',
     },
     migrations: {
-      tableName: 'knex_migrations'
+      directory: './db/migrations',
     },
-    useNullAsDefault: true
+    seeds: {
+      directory: './db/seeds',
+    },
+    useNullAsDefault: true,
   },
 
   staging: {
     client: 'postgresql',
     connection: {
       database: 'my_db',
-      user:     'username',
-      password: 'password'
+      user: 'username',
+      password: 'password',
     },
     pool: {
       min: 2,
-      max: 10
+      max: 10,
     },
     migrations: {
-      tableName: 'knex_migrations'
-    }
+      tableName: 'knex_migrations',
+    },
   },
 
   production: {
     client: 'postgresql',
     connection: {
       database: 'my_db',
-      user:     'username',
-      password: 'password'
+      user: 'username',
+      password: 'password',
     },
     pool: {
       min: 2,
-      max: 10
+      max: 10,
     },
     migrations: {
-      tableName: 'knex_migrations'
-    }
-  }
-
-};
+      tableName: 'knex_migrations',
+    },
+  },
+}
--- server.js
+++ server.js
@@ -1,24 +1,23 @@
 import 'dotenv/config'
-import express from 'express';
+import express from 'express'
 import morgan from 'morgan'
 import cors from 'cors'
-import knex from 'knex'
 
 import db from './db/db.js' //soon to be replaced with sqlite3
-import knexConfig from './db/knexfile.js'
-
+import query from './db/db-config.js'
 const app = express()
 const port = process.env.PORT
-knex(knexConfig[process.env.NODE_ENV])
 
 app.use(cors())
 app.use(morgan('dev'))
 app.use(express.json()) // replaces body-parser
 
-import commentRoutes from "./routes/comments.js"
+import commentRoutes from './routes/comments.js'
 app.use('/comments', commentRoutes(db))
-import userRoutes from "./routes/user.js"
+import userRoutes from './routes/user.js'
 app.use('/user', userRoutes(db))
+import testRoutes from './routes/test.js'
+app.use('/test', testRoutes(query))
 
 app.listen(port, () => {
   console.log('Server running on port ' + port)
