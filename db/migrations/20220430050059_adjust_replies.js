44c251fce23633930669b84cf9f923577bf034bf
Logan Woolf <loganwoolf@gmail.com>
2022-04-29 23:48:12 -0600

    Modify comments table with reply_name column

---
+++ db/migrations/20220430050059_adjust_replies.js
@@ -0,0 +1,17 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+export function up(knex) {
+  return knex.schema.alterTable('comments', (table) => {
+    table.string('reply_name')
+  })
+}
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+export function down(knex) {
+  return knex.schema.dropColumn('reply_name')
+}
