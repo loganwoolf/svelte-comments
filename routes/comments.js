5c4aa8d5fd377706d717add4f88c18113ed61e53
Logan Woolf <loganwoolf@gmail.com>
2022-05-06 20:10:17 -0600

    Create new comment route

--- routes/comments.js
+++ routes/comments.js
@@ -43,5 +43,47 @@ export default (query) => {
       .then((data) => res.json(data))
   })
 
+  router.post('/', (req, res) => {
+    const currentUserID = 4
+    const commentObj = {
+      content: req.body.content,
+      user: currentUserID,
+    }
+
+    query('comments')
+      .insert(commentObj)
+      .then(() => {
+        query('comments')
+          .join('users', 'users.id', 'comments.user')
+          .select({
+            id: 'comments.id',
+            content: 'comments.content',
+            createdAt: 'comments.created_at',
+            score: 'comments.score',
+            username: 'users.username',
+            image: 'users.image_png',
+            replyTo: 'comments.reply_to',
+            replyName: 'comments.reply_name',
+          })
+          .orderBy('created_at', 'desc')
+          .limit(1)
+          .then((rows) => {
+            return rows.map((comment) => {
+              comment.user = {
+                username: comment.username,
+                image: comment.image,
+              }
+              delete comment.username
+              delete comment.image
+              return comment
+            })
+          })
+          .then((rows) => {
+            res.json(rows[0])
+          })
+      })
+      .catch((error) => console.log({ error }))
+  })
+
   return router
 }
