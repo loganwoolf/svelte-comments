50b6572d64c39a3ed98f6615a1e53501f8a8b380
Logan Woolf <loganwoolf@gmail.com>
2022-05-23 01:52:49 -0600

    Add route to reply to comment

--- routes/comments.js
+++ routes/comments.js
@@ -44,9 +44,50 @@ export default (query) => {
   })
 
   router.post('/:id', (req, res) => {
-    
+    const currentUserID = 4
+    const commentObj = {
+      content: req.body.content,
+      reply_name: req.body.replyName,
+      reply_to: req.params.id,
+      user: currentUserID,
+    }
+
+    query('comments')
+      .insert(commentObj)
+      .then(() => {
+        query('comments')
+          .join('users', 'users.id', 'comments.user')
+          .select({
+            id: 'comments.id',
+            content: 'comments.content',
+            createdAt: 'comments.created_at',
+            score: 'comments.score',
+            username: 'users.username',
+            image: 'users.image_png',
+            replyTo: 'comments.reply_to',
+            replyName: 'comments.reply_name',
+          })
+          .orderBy('created_at', 'desc')
+          .limit(1)
+          .then((rows) => {
+            return rows.map((comment) => {
+              comment.user = {
+                username: comment.username,
+                image: comment.image,
+              }
+              comment.replies = []
+              delete comment.username
+              delete comment.image
+              return comment
+            })
+          })
+          .then((rows) => {
+            res.json(rows[0])
+          })
+      })
+      .catch((error) => console.log({ error }))
   })
-  
+
   router.post('/', (req, res) => {
     const currentUserID = 4
     const commentObj = {
