8c92a9a2804eb9573e03eb79f10ba8ce26f79b68
Logan Woolf <loganwoolf@gmail.com>
2022-06-01 00:41:36 -0600

    Add a logged in downvote to default db and format

--- db/seeds/comments.js
+++ db/seeds/comments.js
@@ -13,7 +13,10 @@ export async function seed(knex) {
         "Impressive! Though it seems the drag feature could be improved. But overall it looks incredible. You've nailed the design and the responsiveness at various breakpoints works really well.",
       created_at: '2022-03-28 06:21:05',
       updated_at: '2022-03-28 06:21:05',
-      votes: JSON.stringify({"up_users":[21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33],"down_users":[61]})
+      votes: JSON.stringify({
+        up_users: [21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33],
+        down_users: [61],
+      }),
     },
     {
       id: 2,
@@ -22,7 +25,10 @@ export async function seed(knex) {
         "Woah, your project looks awesome! How long have you been coding for? I'm still new, but think I want to dive into React as well soon. Perhaps you can give me an insight on where I can learn React? Thanks!",
       created_at: '2022-04-14 06:21:05',
       updated_at: '2022-04-14 06:21:05',
-      votes: JSON.stringify({"up_users":[21, 22, 23, 24, 25, 26, 4],"down_users":[61, 62]})
+      votes: JSON.stringify({
+        up_users: [21, 22, 23, 24, 25, 26, 4],
+        down_users: [61, 62],
+      }),
     },
     {
       id: 3,
@@ -33,7 +39,10 @@ export async function seed(knex) {
       updated_at: '2022-04-21 06:21:05',
       reply_name: 'maxblagun',
       reply_to: 2,
-      votes: JSON.stringify({"up_users":[21, 22, 23, 24, 25],"down_users":[61]})
+      votes: JSON.stringify({
+        up_users: [21, 22, 23, 24, 25],
+        down_users: [61, 4],
+      }),
     },
     {
       id: 4,
@@ -44,7 +53,7 @@ export async function seed(knex) {
       updated_at: '2022-04-27 06:21:05',
       reply_name: 'ramsesmiron',
       reply_to: 2,
-      votes: JSON.stringify({"up_users":[21],"down_users":[61, 62, 63]})
+      votes: JSON.stringify({ up_users: [21], down_users: [61, 62, 63] }),
     },
   ])
 }
